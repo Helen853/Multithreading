f0475b99f582dae191a6edc1ba7b8617e817e5e4
Киса Мурлыса <kisamurlysa@MacBook-Air-Ola.local>
2024-03-19 01:28:58 +0500

    Commit

---
+++ Multithreading/Multithreading/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Multithreading
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+
+
+}
+
---
+++ Multithreading/Multithreading/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Multithreading
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
